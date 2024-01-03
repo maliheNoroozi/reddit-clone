583775bea84505ae51a2184363debb60dbb2ec40
Maliheh Norouzi <malihe.noroozi64@yahoo.com>
2024-01-03 11:00:04 +0100

    Fixed some style

--- src/app/(non-auth)/topics/[slug]/page.tsx
+++ src/app/(non-auth)/topics/[slug]/page.tsx
@@ -23,19 +23,19 @@ export default async function TopicDetailsPage({ params }: TopicDetailsProps) {
   }
 
   return (
-    <div className="w-full h-full grid grid-cols-4 gap-4">
+    <div className="w-full h-full grid grid-cols-1 auto-rows-auto gap-y-4 md:grid-cols-4 md:gap-x-4 content-start">
       <div className="col-span-3">
         <h1 className="text-2xl mb-2">{topic.slug}</h1>
         <PostList fetchData={() => fetchPostsByTopicSlug(topic.slug)} />
       </div>
-      <div className="shadow border px-2 pt-2 flex flex-col gap-2 h-80">
+      <div className="col-span-1 shadow border p-4 flex flex-col gap-4 h-fit">
         <CreatePostForm topicSlug={params.slug} />
         <Divider />
-        <div>
+        <div className="min-h-60 max-h-96 overflow-auto">
           <h2 className="font-bold text-xl flex items-center justify-center pb-2">
             {topic.slug}
           </h2>
-          <div>{topic.description}</div>
+          <p>{topic.description}</p>
         </div>
       </div>
     </div>
